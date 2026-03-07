79b254032dc024fb836451cf0249d8bf75812130
TACO <ch85221@gmail.com>
2026-03-07 23:29:25 +0800

    <更改訊息>

--- main.py
+++ main.py
@@ -1,46 +1,76 @@
 import random
-money = 1000
-current_round = 1
 
+current_round = {"round": 0}
+money = {"money": 0}
 backpack = {"A": 0, "B": 0}
 prices = {"A": 0, "B": 0}
 
-while current_round <= 5:
-    prices["A"] = random.randint(100, 200)
-    prices["B"] = random.randint(100, 200)
-    
-    print(f"ROUND: {current_round}")
-    print(f"今日市價: {prices}")
-    print(f"剩餘資金: {money}  目前背包: {backpack}")
 
-    action = input("(1)買入 (2)賣出 (3)跳過: ")
-    
-    if action == "3":
-        current_round += 1
-        continue 
 
-    target = input("請輸入貨物名稱 (A/B): ").upper()
+def init():
+    current_round["round"] = 1
+    money["money"] = 1000
+    backpack["A"] = 0
+    backpack["B"] = 0
+    prices["A"] = 0
+    prices["B"] = 0
+    print("遊戲已重置！")
 
-    if target in backpack:
-        num = int(input(f"多少個 {target}？: "))
-        
-        if action == "1":  
-            cost = num * prices[target] 
-            if cost <= money:
-                money -= cost
-                backpack[target] += num
-                print(f"成功買入 {num} 個 {target}")
-            else:
-                print("餘額不足！")
-
-        elif action == "2":
-            if num <= backpack[target]:
-                money += num * prices[target]
-                backpack[target] -= num
-                print(f"成功賣出 {num} 個 {target}")
-            else:
-                print("庫存不足！")
+def buy(target: str, num: int)  -> str:
+    cost = num * prices[target]
+    if cost <= money["money"]:
+        money["money"] -= cost
+        backpack[target] += num
+        return f"成功買入 {num} 個 {target}"          
     else:
-        print("沒有這種東西！")
+        return "餘額不足！"
 
-    current_round += 1
+def sell(target: str, num: int)  -> str:
+    cost = num * prices[target]
+    if num <= backpack[target]:
+        money["money"] += cost
+        backpack[target] -= num
+        return f"成功賣出 {num} 個 {target}"          
+    else:
+        return "存貨不足！"
+
+
+def game_loop():
+    while current_round["round"] <= 5:
+        prices["A"] = random.randint(100, 200)
+        prices["B"] = random.randint(100, 200)
+        while True:
+            print(f"\nROUND: {current_round}")
+            print(f"今日市價: {prices}")
+            print(f"剩餘資金: {money['money']}  目前背包: {backpack}")
+
+            action = input("(1)買入 (2)賣出 (3)結束這回合: ")
+            
+            if action == "1":
+                target = input("請輸入貨物名稱 (A/B): ").upper()
+                if target not in prices:
+                    print("無效貨物名稱！")
+                    continue
+                num = int(input(f"想要買多少個 {target}？: "))
+                print(buy(target, num))
+            elif action == "2":
+                target = input("請輸入貨物名稱 (A/B): ").upper()
+                if target not in prices:
+                    print("無效貨物名稱！")
+                    continue
+                num = int(input(f"想要賣多少個 {target}？: "))
+                print(sell(target, num))
+            elif action == "3":
+                current_round["round"] += 1
+                continue
+            else:
+                print("無效指令")
+                continue
+
+        
+    
+    return"\n遊戲結束！" f"最終資產: {money['money']}"   
+
+if __name__ == "__main__":
+    init()
+    game_loop()
